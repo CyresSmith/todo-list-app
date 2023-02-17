db90d4a67960d543450d5db94087b2f53805f15f
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-02-17 18:33:45 +0200

    init withSlice

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux/es/exports';
-import { addTask } from 'redux/actions';
+import { addTask } from 'redux/taskSlice';
 
 import Button from '../Button';
 import css from './TaskForm.module.css';
